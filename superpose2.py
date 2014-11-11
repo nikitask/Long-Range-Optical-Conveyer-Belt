fb10abe5f34e2a4e7ceab61c40cb8bfe3b68d587
nikitask <nikitask.23@gmail.com>
2014-11-11 13:48:13 -0500

    Update superpose2.py

--- superpose2.py
+++ superpose2.py
@@ -1,4 +1,10 @@
-def superpose2(phi1, phi2, alpha, amp1 = [], amp2 = [], ampout = []):
+import scipy as sp
+import random
+import numpy as np
+
+def superpose2(phi1, phi2, alpha=1, amp1 = [], amp2 = [], ampout = []):
+    phi1 = np.array(phi1)
+    phi2 = np.array(phi2)
     tempshape = phi1.shape
     w = tempshape[1]
     h = tempshape[0]
@@ -12,3 +18,11 @@ def superpose2(phi1, phi2, alpha, amp1 = [], amp2 = [], ampout = []):
         temp2 = ones(h)
         for r in temp2:
             amp2 += [temp1]
+    psi = (amp1)*np.exp(1j*phi1) + alpha*(amp2)*np.exp(1j*phi2)
+    psi = psi/(np.amax(abs(psi)))
+    phi = np.arctan2(sp.real(psi),sp.imag(psi))
+    phi -= np.amin(phi)
+    phi = phi % (2.*np.pi)
+    eta =2*np.median(abs(psi))
+    ampout = abs(psi)
+    return phi, ampout
