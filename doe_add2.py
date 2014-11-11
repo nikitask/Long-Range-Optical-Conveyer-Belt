1e92310b99f8e2737fd7b9846d0ac0f2f2bd9368
nikitask <nikitask.23@gmail.com>
2014-11-11 13:47:24 -0500

    Update doe_add2.py

--- doe_add2.py
+++ doe_add2.py
@@ -16,8 +16,8 @@ def doe_add2(phi1, phi2, nomod = 0, amp1 =[], amp2=[], eta = 0, ampout= 0): #doe
 			for r in temp2:
 				amp1 += [temp1]
 		if len(amp2) == 0:
-			temp1 = ones(w)
-			temp2 = ones(h)
+			temp1 = np.ones(w)
+			temp2 = np.ones(h)
 			for r in temp2:
 				amp2 += [temp1]
 		psi1 = amp1 * np.exp(1j*phi1)
