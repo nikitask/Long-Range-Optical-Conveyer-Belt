309699a16a6e3cd415ed8abbf4d85e3c17206697
nikitask <nikitask.23@gmail.com>
2014-12-09 14:32:04 -0500

    Turned projectconveyer into a function
    It is now incorporated within SLMControl.py

--- projectconveyer.py
+++ projectconveyer.py
@@ -6,12 +6,11 @@
 import holo_to_slm,superpose,doe_add,besselbeam,displace
 import qimage2ndarray as q2
 
-amp1, phi1 = besselbeam.besselbeam(30)
-amp2, phi2 = besselbeam.besselbeam(20)
-print(amp1.shape, amp2.shape, phi1.shape, phi2.shape)
-phi, ampout = superpose.superpose(phi1, phi2, amp1 = amp1, amp2 = amp2)
-phi3 = displace.displace(140,10)
-#displace with w = 1080, h = 1920 returns phi.shape(1920,1080)
-print("THIS IS PHI", phi.shape, phi3.shape)
-phiout = doe_add.doe_add(phi,phi3, amp1 = ampout)#removed amp2
-holo_to_slm.main(phiout)
+def projectconveyer(phase = 0):
+  amp1, phi1 = besselbeam.besselbeam(30)
+  amp2, phi2 = besselbeam.besselbeam(20)
+  print(amp1.shape, amp2.shape, phi1.shape, phi2.shape)
+  phi, ampout = superpose.superpose(phi1 + phase, phi2, amp1 = amp1, amp2 = amp2)
+  phi3 = displace.displace(140,10)
+  phiout = doe_add.doe_add(phi,phi3, amp1 = ampout)#removed amp2
+  return phiout
