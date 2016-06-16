e981ae84ad3b1436ec234e7d6eb139b3fbcbe1b6
nikitask <nikitask.23@gmail.com>
2016-06-16 13:39:02 -0400

    changed function calls to "construct"

--- projectconveyor.py
+++ projectconveyor.py
@@ -3,14 +3,14 @@
 
 
 
-import holo_to_slm,superpose,doe_add,besselbeam,displace
+import holo_to_slm,superpose,doe_add,bessel,displace
 import qimage2ndarray as q2
 
 def projectconveyor(phase = 0):
-  amp1, phi1 = besselbeam.besselbeam(30)
-  amp2, phi2 = besselbeam.besselbeam(20)
+  amp1, phi1 = bessel.construct(30)
+  amp2, phi2 = bessel.construct(20)
   print(amp1.shape, amp2.shape, phi1.shape, phi2.shape)
-  phi, ampout = superpose.superpose(phi1 + phase, phi2, amp1 = amp1, amp2 = amp2)
-  phi3 = displace.displace(140,10)
-  phiout = doe_add.doe_add(phi,phi3, amp1 = ampout)#removed amp2
+  phi, ampout = superpose.construct(phi1 + phase, phi2, amp1 = amp1, amp2 = amp2)
+  phi3 = displace.construct(140,10)
+  phiout = doe_add.construct(phi,phi3, amp1 = ampout)#removed amp2
   return phiout
